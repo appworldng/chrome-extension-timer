95c6a052d0945341a2a03b4c70edd9055c43833c
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-13 07:58:37 +0100

    Modified popup.js with timer object method

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 let changeColor = document.getElementById('changeColor');
+//let timer = document.getElementById('chrome-extension-timer');
 
 chrome.storage.sync.get('color', function(data) {
     changeColor.style.backgroundColor = data.color;
@@ -7,10 +8,12 @@ chrome.storage.sync.get('color', function(data) {
 
 changeColor.onclick = function(element) {
     let color = element.target.value;
+    //alert("ok");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.executeScript(
           tabs[0].id,
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
+    //timer.style.display = 'block';
 };
 
